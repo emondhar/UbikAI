7b1de8315ff8d3f8c8c6afc438a913d0fe550f37
emondhar <emondhar514@gmail.com>
2021-06-12 11:15:14 -0400

    everything is working

---
+++ project/frontend/src/components/HomePage.js
@@ -0,0 +1,30 @@
+import React, { Component } from "react";
+import RoomJoinPage from "./RoomJoinPage";
+import CreateRoomPage from "./CreateRoomPage";
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link,
+    Redirect,
+} from "react-router-dom";
+
+export default class HomePage extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <Router>
+                <Switch>
+                    <Route exact path="/">
+                        <h3>This is the home page</h3>
+                    </Route>
+                    <Route path="/join" component={RoomJoinPage} />
+                    <Route path="/create" component={CreateRoomPage} />
+                </Switch>
+            </Router>
+        );
+    }
+}
